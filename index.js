cc21179b7a3a6c937555031593efa1640859c176
Marie <marie.kasai@live.co.uk>
2015-09-30 15:07:51 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-//comments
+var Http = require('http');
 
-something.
+var port = 4000;
+
+function handler(req, res) {
+  console.log(process.env.PORT);
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end("<h1>WOAH</h1>");
+}
+
+Http.createServer(handler).listen(port);
